a905513f4ff5e7fc8d55efde19d59f39fb24720d
jdm79 <james.malvern@gmail.com>
2019-01-15 13:28:12 +0000

    first commit

---
+++ swift/nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
@@ -0,0 +1,17 @@
+struct DNA {
+  
+  init(strand:String) {
+
+  }
+
+  // a func that counts how many of a particular nucleotide there is in a given strand
+  // set an int to 0, add 1 each time a matching nucleotide is counted
+  func count(nucleotide:String) {
+
+  }
+
+  // a func that
+  func counts() {
+
+  }
+}//Solution goes in Sources
