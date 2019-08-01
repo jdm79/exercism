d6dd3650fa9c606988890aa4767e2ffe1299b6a9
jdm79 <james.malvern@gmail.com>
2019-08-01 16:35:16 +0100

    solution to two-fer R

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,6 +1,4 @@
 def two_fer(name = "you"):
-  if name:
-    return "One for {}, one for me.".format(name)
-  else:
-    return 'One for you, one for me.'
+  return "One for {}, one for me.".format(name)
+  
 
